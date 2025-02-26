ef7ec094c66ce6e2ad4ed66c45dd6fe7d21b0e91
Manolo Sainas <109757444+ManoloSainas@users.noreply.github.com>
2025-02-26 18:08:45 +0100

    rana funzionante

--- versione_thread/rana.c
+++ versione_thread/rana.c
@@ -1,9 +1,9 @@
 #include "frogger.h"
 
-void *rana(void *arg)
+void *
+rana(void *arg)
 {
     elementoGioco ranaGiocatore;
-
     info_elemento *info_rana = (info_elemento *)arg;
     ranaGiocatore.tipo = RANA;
     ranaGiocatore.x = info_rana->x;
@@ -13,10 +13,11 @@ void *rana(void *arg)
     ranaGiocatore.direzione = info_rana->direzione;
     ranaGiocatore.proiettile = INVALID_THREAD;
 
-    // Ensure controllo_rana is properly initialized
     bool controllo_rana = true;
     ranaGiocatore.controllo = &controllo_rana;
 
+    // Impostiamo nodelay per evitare che wgetch blocchi
+    nodelay(gioco, TRUE);
     keypad(gioco, TRUE);
 
     int num_spari = 0; // contatore per la gestione della quantità di spari
@@ -25,11 +26,11 @@ void *rana(void *arg)
     {
         int c;
 
-        // Lock the mutex before calling wgetch and modifying shared resources
+        // Usiamo un mutex per proteggere l'accesso a wgetch()
         lock_mutex();
         c = wgetch(gioco);
+        unlock_mutex();
 
-        // Gestione movimento rana
         switch (c)
         {
         case KEY_UP:
@@ -48,42 +49,17 @@ void *rana(void *arg)
             if (ranaGiocatore.x < maxx - 3)
                 ranaGiocatore.x += SPOSTAMENTO_RANA;
             break;
-            // Gestione sparo granate
-            // case KEY_SPACE:
-            //     if (num_spari <= MAXGRANATE - 2) // controllo per la quantità di spari
-            //     {
-            //         num_spari++;
-            //         // creazione thread proiettile sinistro
-
-            //         num_spari++;
-            //         // creazione thread proiettile destro
-            //         // pthread_t thread_proiettile_destro;
-            //         // if (pthread_create(&thread_proiettile_destro, NULL, proiettile, &ranaGiocatore) != 0)
-            //         // {
-            //         //     perror("Errore creazione thread proiettile destro");
-            //         // }
-            //     }
-            //     break;
         default:
             break;
         }
 
-        // if (ranaGiocatore.y < maxy - 2 && ranaGiocatore.y > miny + 5)
-        // {
-        //     ranaGiocatore.direzione = flussi[16 - ranaGiocatore.y].direzione; // direzione del flusso
-        //     ranaGiocatore.velocita = flussi[16 - ranaGiocatore.y].velocita;   // velocità del flusso
-        // }
-
-        // ranaGiocatore.proiettile = num_spari; // sfruttiamo la sezione inuttilizzata per debugging e controlli aggiuntivi
-
-        // Scrittura nella lista thread delle informazioni della rana
         wait_produttore();
         lista_elementi[in] = ranaGiocatore;
         in = (in + 1) % DIM_BUFFER;
         signal_produttore();
 
-        // Unlock the mutex after modifying shared resources
-        unlock_mutex();
+        // Evitiamo di usare la CPU al 100%
+        usleep(50000);
     }
     return NULL;
 }
